e8849860f719d12db1d567e6bca95a76167b0848
jee00609 <31675804+jee00609@users.noreply.github.com>
2017-11-27 10:57:05 +0900

    두번째
    비쥬얼 시 오류
    MINGW 시 오류 X

---
+++ arraymalloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int *ary=NULL;
+	int i=0,n=0,sum=0;
+
+	if((ary=(int*)malloc(sizeof(int)))==NULL)
+	{
+		printf("메모리 할당에 문제가 있습니당ㅎㅎ.\n");
+		exit(1);
+	}
+
+	printf("입력할 점수의 개수를 입력 >>> ");
+	scanf("%d",&n);
+
+	printf("%d개의 점수를 입력 >>> ",n);
+
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(ary+i));
+		sum += *(ary+i);
+	}
+	
+	printf("합:%d 평균:%.1f\n",sum,(double)sum/n);
+
+	free(ary);
+
+	return 0;
+
+}
